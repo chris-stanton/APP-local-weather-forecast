c45ee6336e993dbf6ea769b5e0b266cf42d92c3a
Chris Stanton <cstanton0760@yahoo.com>
2017-08-16 15:32:05 -0500

    frst commit

---
+++ README.md
@@ -0,0 +1,40 @@
+
+# Dark Sky API Weather App
+
+---
+
+## Description
+##### Simple weather app that uses Dark Sky's API. User's location is automatically found on init through the browser. The background picture and weather data icon change to the current weather and time of day. User can view weather data in standard or metric values.  
+
+<img src="./public/assets/images/desktop-one.png" width="50%">
+
+---
+
+## Technologies Used
+  1. HTML
+  2. CSS
+  3. Javascript
+  4. Bootstrap
+  5. jQuery
+  6. Font-awesome
+
+---  
+
+## How To Install App
+  1. Download zip file
+  2. Open index.html file in browser
+
+---
+
+#### App can be viewed at: https://darksky-weather.herokuapp.com/
+
+---
+
+## License
+##### Copyright 2017 Chris Stanton
+
+###### Permission is hereby granted, free of charge, to any person obtaining a copy of this software and associated documentation files (the "Software"), to deal in the Software without restriction, including without limitation the rights to use, copy, modify, merge, publish, distribute, sublicense, and/or sell copies of the Software, and to permit persons to whom the Software is furnished to do so, subject to the following conditions:
+
+###### The above copyright notice and this permission notice shall be included in all copies or substantial portions of the Software.
+
+###### THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
---
+++ public/scripts/script.js
@@ -0,0 +1,65 @@
+var weatherObj = {};
+
+$(document).ready(function() {
+  var long = 0;
+  var lat = 0;
+  if (navigator.geolocation) {
+    navigator.geolocation.getCurrentPosition(function(position) {
+      lat = position.coords.latitude;
+      long = position.coords.longitude;
+      var url = 'https://api.darksky.net/forecast/48429d15c51d64e8e6c03a84bec4e5b8/' + lat + ',' + long;
+      console.log(url);
+
+      $.ajax({
+        type: "GET",
+        url: url,
+        dataType: "jsonp",
+        success: function(response) {
+          console.log(response);
+          $('.location').html(response.timezone);
+          //check postman to see if these are correctly referenced
+          $('.humidity').html(((Math.round(response.currently.humidity)) * 10) + '%');
+          $('.windspeed').html((response.currently.windSpeed) + ' mph');
+          $('.temp').html(Math.round(response.currently.temperature) + ' °F');
+          $('.current-summary').html(response.currently.summary)
+          console.log(response.currently);
+
+          if (response.currently.icon === "clear-day") {
+            $('.icon').html('<img src="https://png.icons8.com/clouds/Dusk_Wired/64">');
+            $('body').css('background-image', "url(http://crevisio.com/images/posts/96/yjf9TgZEw/Crevisio-96-yjf9TgZEw.jpg)");
+          } else if (response.currently.icon === "clear-night") {
+            $('.icon').html('<img src="https://image.flaticon.com/icons/svg/17/17279.svg">');
+            $('body').css('background-image', "url(http://clear-night.com/img/clear-night-placeholder.jpg)");
+            $('body').css('color', "white");
+          } else if (response.currently.icon === "rain") {
+            $('.icon').html('<img src="https://www.iconexperience.com/_img/o_collection_png/green_dark_grey/512x512/plain/cloud_rain.png">');
+            $('body').css('background-image', "url(https://static.pexels.com/photos/8486/water-rain-raindrops-drops.jpg)");
+            $('body').css('color', "white");
+          } else if (response.currently.icon === "snow") {
+            $('.icon').html('<img src="https://cdn2.iconfinder.com/data/icons/lil-weather/161/snowflake-512.png">');
+            $('body').css('background-image', "url(https://vignette4.wikia.nocookie.net/phobia/images/a/aa/Snow.jpg/revision/latest?cb=20161109045734)");
+          } else if (response.currently.icon === "sleet") {
+            $('.icon').html('<img src="http://icons.iconarchive.com/icons/icons8/android/256/Weather-Sleet-icon.png">');
+            $('body').css('background-image', "url(http://www.abccolumbia.com/wp-content/uploads/2016/01/Image4.jpg)");
+          } else if (response.currently.icon === "wind") {
+            $('.icon').html('<img src="https://cdn1.iconfinder.com/data/icons/weather-18/512/wind_storm-512.png">');
+            $('body').css('background-image', "url(https://media.mnn.com/assets/images/2016/05/01-windydog-carloscherer.jpg.838x0_q80.jpg)");
+          } else if (response.currently.icon === "fog") {
+            $('.icon').html('<img src="https://cdn4.iconfinder.com/data/icons/heavy-weather/100/Weather_Icons_09_fog-512.png">');
+            $('body').css('background-image', "url(https://www.howitworksdaily.com/wp-content/uploads/2014/08/fog-06.jpg)");
+          } else if (response.currently.icon === "cloudy") {
+            $('.icon').html('<img src="http://www.endlessicons.com/wp-content/uploads/2012/12/cloudy-icon-614x460.png">');
+            $('body').css('background-image', "url(http://www.quotemaster.org/images/d9/d982a47690bc30527138826a3b13206e.jpg)");
+          } else if (response.currently.icon === "partly-cloudy-day") {
+            $('.icon').html('<img src="http://www.endlessicons.com/wp-content/uploads/2012/12/mostly-cloudy-icon-614x460.png">');
+            $('body').css('background-image', "url(https://ak9.picdn.net/shutterstock/videos/9503747/thumb/1.jpg)");
+          } else if (response.currently.icon === "partly-cloudy-night") {
+            $('.icon').html('<img src="http://icons.veryicon.com/ico/System/Icons8%20Metro%20Style/Weather%20Partly%20cloudy%20night.ico">');
+            $('body').css('background-image', "url(http://cdn.weatheravenue.com/img/background/background-night.jpg)");
+            $('body').css('color', "white");
+          }
+        } // end success
+      }); // end ajax
+    }); // end get current position
+  } // end geolocation
+}); // end document ready function
